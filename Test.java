52af2b71ebfe2dd763cc83ebaf9f24fc27b2762a
Chanakya-dev <Chanakyamanas123@gmail.com>
2024-07-23 14:35:23 +0530

    Create Test.java

---
+++ Test.java
@@ -0,0 +1,11 @@
+public class Test{
+
+  public static void main(String[]args){
+// prepare an object of Bank Account with no paramaters and initalize the values using getters and setters
+    // call the functions and print the deposit withdrawl and account details
+    // prepare an object of Bank Account with paramaters
+    // Call the Functions and print values
+
+}
+
+}
