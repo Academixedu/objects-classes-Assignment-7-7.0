332a25c4404bffa00d3f0477fedd81f26500e8e1
Shreyash000007 <101011157+Shreyash000007@users.noreply.github.com>
2024-07-23 20:13:00 +0530

    Update BankAccount.java

--- BankAccount.java
+++ BankAccount.java
@@ -6,9 +6,62 @@ class BankAccount {
         this.accountNumber = accountNumber;
         this.balance = initialBalance;
     }
+    public BankAccount() 
+    {
+
+    }
+
 
   // prepare deposit logic in void deposit function
+  public void deposit(int amount)
+  {
+     balance+=amount;
+    System.out.println("Successfully Deposited = "+balance);
+  }
   // prepare withdraw logic in void witdraw function
+  public void withdraw(int amount)
+  {
+    if(balance<=0)
+    {
+        balance-=amount;
+        System.out.println("Successfull withdraw "+balance);
+    }
+    else if(balance<amount)
+    {
+        System.out.println("Insufficient Balnce");
+    }
+    else 
+    {
+        System.out.println("zero Balance");
+    }
+  }
   // prepare displayAccountDetails logic and ensure that it should contain current Balance and accountNumber in print statements
+  public void displayAccountDetails()
+  {
+    System.out.println("Account Number = "+accountNumber);
+    System.out.println("Current Balance = "+balance);
+  }
   // prepare getters and setters
+  
+
+  public String getAccountNumber() {
+    return accountNumber;
+  }
+
+
+  public void setAccountNumber(String accountNumber) {
+    this.accountNumber = accountNumber;
+  }
+
+
+  public double getBalance() {
+    return balance;
+  }
+
+
+  public void setBalance(double balance) {
+    this.balance = balance;
+  }
+  
+
    }
