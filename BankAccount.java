054e1050c70da28f2e3515d03ee78a81cb191089
Chanakya-dev <Chanakyamanas123@gmail.com>
2024-07-23 14:31:04 +0530

    Create BankAccount.java

---
+++ BankAccount.java
@@ -0,0 +1,18 @@
+class BankAccount {
+    private String accountNumber;
+    private double balance;
+
+    public BankAccount(String accountNumber, double initialBalance) {
+        this.accountNumber = accountNumber;
+        this.balance = initialBalance;
+    }
+
+  // prepare deposit logic in void deposit function
+  // prepare withdraw logic in void witdraw function
+  // prepare displayAccountDetails logic and ensure that it should contain current Balance and accountNumber in print statements
+  
+    public static void main(String[] args) {
+        //take Argumented Constructor here 
+        // Call All the Methods here
+    }
+}
