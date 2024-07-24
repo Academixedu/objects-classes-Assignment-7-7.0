3e1553dc4f6d712d0405e12f42e65de394ce442e
nayeem6836 <nayeemmaseed9100@gmail.com>
2024-07-24 17:40:49 +0530

    commit

--- Test.java
+++ Test.java
@@ -5,7 +5,14 @@ public class Test{
     // call the functions and print the deposit withdrawl and account details
     // prepare an object of Bank Account with paramaters
     // Call the Functions and print values
+    BankAccount account = new BankAccount("123456789", 1000.0);
 
+        account.displayAccountDetails();
+
+        account.deposit(500.0); 
+        account.withdraw(200.0);
+
+        account.withdraw(1500.0);
 }
 
 }
