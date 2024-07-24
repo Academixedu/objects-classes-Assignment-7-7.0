69a2e33d94000a31e332d6da8de625e3617b8d25
syamkuamrnallamilli0 <syamkumarnallamilli0@gmail.com>
2024-07-24 17:31:20 +0530

    commit

--- BankAccount.java
+++ BankAccount.java
@@ -1,14 +1,67 @@
-class BankAccount {
+public class BankAccount {
     private String accountNumber;
     private double balance;
+    
+    
 
     public BankAccount(String accountNumber, double initialBalance) {
         this.accountNumber = accountNumber;
         this.balance = initialBalance;
     }
+//test.java
+    public BankAccount(){
+        
+    }
+
 
   // prepare deposit logic in void deposit function
+    public void deposit(double amount){
+        balance=balance+amount;
+        System.out.println("deposit:" +amount);
+    }
   // prepare withdraw logic in void witdraw function
+    public void withdraw(double amount){
+        balance=balance-amount;
+        System.out.println("withdraw:"+amount);
+    }
   // prepare displayAccountDetails logic and ensure that it should contain current Balance and accountNumber in print statements
+
+
+
+    public void displayAccountDetails(){
+        System.out.println("accountnumber:"+accountNumber);
+        System.out.println("current balance:"+balance);
+    }
+
+
   // prepare getters and setters
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+    public double getBalance() {
+        return balance;
+    }
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+        public static void main(String[] args) {
+            BankAccount obj=new BankAccount("123456789", 500000);
+
+                // System.out.println("accountnumber:"+obj.getAccountNumber());
+                // System.out.println("current balance:"+obj.getBalance());
+            obj.deposit(200000);
+            obj.withdraw(100000);
+            obj.displayAccountDetails();
+
+            // System.out.println("acc.no:" +obj.getAccountNumber()+ " " + "balance:" +obj.getBalance());
+            
+            obj.setAccountNumber("67456457564");
+            obj.setBalance(2000);
+            obj.displayAccountDetails();
+
+            
+        }
    }
--- Test.java
+++ Test.java
@@ -5,6 +5,25 @@ public class Test{
     // call the functions and print the deposit withdrawl and account details
     // prepare an object of Bank Account with paramaters
     // Call the Functions and print values
+    BankAccount obj1=new BankAccount();
+    obj1.setAccountNumber("758437898");
+    obj1.setBalance(2000);
+
+    System.out.println("acc.no:" +obj1.getAccountNumber()+ " " + "balance:" +obj1.getBalance());
+    obj1.deposit(4000);
+    obj1.withdraw(2000);
+    obj1.displayAccountDetails();
+    // System.out.println("acc.no:" +obj1.getAccountNumber()+ " " + "balance:" +obj1.getBalance());
+
+    BankAccount obj2=new BankAccount("8745873563", 5000);
+    obj2.displayAccountDetails();
+    obj2.deposit(10000);
+    obj2.withdraw(5000);
+    obj2.displayAccountDetails();
+
+
+    
+
 
 }
 
