158f63a0d1c44f58261d58ae337ff8e379e669d7
syamkarthik0 <dharmavarapukarthik@gmail.com>
2024-07-24 15:07:14 +0530

    commit

--- Test.java
+++ Test.java
@@ -1,11 +1,18 @@
 public class Test{
-
   public static void main(String[]args){
-// prepare an object of Bank Account with no paramaters and initalize the values using getters and setters
-    // call the functions and print the deposit withdrawl and account details
-    // prepare an object of Bank Account with paramaters
-    // Call the Functions and print values
-
-}
-
+    BankAccount account1 = new BankAccount("",0);
+    account1.setAccountNumber("SBIN3642123");
+    account1.setBalance(2000);
+    account1.deposit(400);
+    account1.withdraw(200);
+    System.out.println("\nDetails of Account Number1:");
+    account1.displayAccountDetails();
+    
+    BankAccount account2 = new BankAccount("SBIN234568", 1000);
+    System.out.println("-----------------");
+    account2.deposit(100);
+    account2.withdraw(200);
+    System.out.println("\nDetails of Account Number2:");
+    account2.displayAccountDetails();
+  }
 }
