70ee35fcc8fcee6423f126949dd9a229c0d9dd27
Manisugguna0 <subrahmanyamanikanta@gmail.com>
2024-07-24 17:24:16 +0530

    Mani

--- BankAccount.java
+++ BankAccount.java
@@ -6,6 +6,61 @@ class BankAccount {
         this.accountNumber = accountNumber;
         this.balance = initialBalance;
     }
+    
+
+    public BankAccount() {
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+    }
+
+
+    public void deposit(double amount)
+    {
+        if(balance >=0)
+        {
+            balance = balance + amount;
+            System.out.println("total balance is "+balance);
+        }
+        else
+        {
+            System.out.println("not deposited");
+        }
+    }
+    public void withdraw(double amount)
+    {
+        if( balance > amount)
+        {
+            balance = balance - amount;
+            System.out.println("Amount is withdraw successfully"+balance);
+        }
+        else
+        {
+            System.out.println("Amount is not sufficient to withdraw");
+        }
+    }
+    public void details()
+    {
+        System.out.println("account number is "+accountNumber);
+        System.out.println("Balance is "+balance);
+    }
+
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
 
   // prepare deposit logic in void deposit function
   // prepare withdraw logic in void witdraw function
