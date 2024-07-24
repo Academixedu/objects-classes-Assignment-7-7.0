5477c17cc285c17d397018728f929609ea1fb643
SriSankeerthanaGorrela <sankeerthanagorrela@gmail.com>
2024-07-24 17:26:10 +0530

    change

--- BankAccount.java
+++ BankAccount.java
@@ -6,9 +6,54 @@ class BankAccount {
         this.accountNumber = accountNumber;
         this.balance = initialBalance;
     }
+    //Parameter less constructor
+    public BankAccount(){
 
-  // prepare deposit logic in void deposit function
-  // prepare withdraw logic in void witdraw function
-  // prepare displayAccountDetails logic and ensure that it should contain current Balance and accountNumber in print statements
-  // prepare getters and setters
+    }
+    // prepare deposit logic in void deposit function
+    public void deposit(double amount){
+    
+        balance=balance+amount;
+        System.out.println("deposit : "+amount);
+    }
+     // prepare withdraw logic in void witdraw function
+    public void withdrawl(double amount){
+        balance=balance-amount;
+        System.out.println("withdrawl : "+amount);
+    
+    }
+// prepare getters and setters
+public String getAccountNumber() {
+    return accountNumber;
+}
+public void setAccountNumber(String accountNumber) {
+    this.accountNumber = accountNumber;
+}
+public double getBalance() {
+    return balance;
+}
+public void setBalance(double balance) {
+    this.balance = balance;
+}
+ // prepare displayAccountDetails logic and ensure that it should contain current Balance and accountNumber in print statements
+public void displayAccountDetails(){
+    System.out.println("account number : "+accountNumber);
+    System.out.println("balance is :"+balance);
+}
+public static void main(String args[]){
+    BankAccount obj=new BankAccount("19A31A04D4", 50000);
+    // prepare displayAccountDetails logic and ensure that it should contain current Balance and accountNumber in print statements
+    
+    obj.deposit(10000);
+    obj.withdrawl(20000);
+    obj.displayAccountDetails();
+    obj.setBalance(100000);
+    obj.deposit(20000);
+    obj.withdrawl(25000);
+    obj.displayAccountDetails();
+
+    
+
+
+}
    }
