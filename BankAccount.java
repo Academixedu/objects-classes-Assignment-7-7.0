c4f592164e6cdbfa1e621d1af999b43de90275e1
goutham120203 <gouthampaladugu12@gmail.com>
2024-07-24 20:41:40 +0530

    goutham

--- BankAccount.java
+++ BankAccount.java
@@ -7,8 +7,42 @@ class BankAccount {
         this.balance = initialBalance;
     }
 
+    public BankAccount(){
+
+    }
+
   // prepare deposit logic in void deposit function
+  public void deposit(double amount,double balance){
+        balance = balance + amount;
+        System.out.println("the balance is :"+balance);
+    }
   // prepare withdraw logic in void witdraw function
+  public void withdraw(double withdraw,double balance){
+    withdraw = balance - withdraw;
+    System.out.println("the balance :"+balance);
+  }
   // prepare displayAccountDetails logic and ensure that it should contain current Balance and accountNumber in print statements
+  public void displayAccountDetails() {
+        System.out.println("Account Number: " + accountNumber);
+        System.out.println("Current Balance: " + balance);
+    }
   // prepare getters and setters
-   }
+
+public String getAccountNumber() {
+    return accountNumber;
+}
+
+public void setAccountNumber(String accountNumber) {
+    this.accountNumber = accountNumber;
+}
+
+public double getBalance() {
+    return balance;
+}
+
+public void setBalance(double balance) {
+    this.balance = balance;
+}
+  
+}
+
--- Test.java
+++ Test.java
@@ -2,10 +2,18 @@ public class Test{
 
   public static void main(String[]args){
 // prepare an object of Bank Account with no paramaters and initalize the values using getters and setters
+    BankAccount b = new BankAccount();
+    b.setAccountNumber("Goutham");
+    b.setBalance(1000.0);
+    System.out.println("the account number is "+b.getAccountNumber());
+    System.out.println("the balance is "+b.getBalance());
     // call the functions and print the deposit withdrawl and account details
+    b.deposit(100, b.getBalance());
+    b.withdraw(200, b.getBalance());
     // prepare an object of Bank Account with paramaters
+    BankAccount b1 = new BankAccount("goutham", 10000);
     // Call the Functions and print values
+    b1.displayAccountDetails();
 
 }
-
 }
