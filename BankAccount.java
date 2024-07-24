2fa96e9dc3d21fa7a08d1510e10b68b66a85cb0b
preetham0223 <sagilinoyal@gmail.com>
2024-07-24 18:14:14 +0530

    commit

--- BankAccount.java
+++ BankAccount.java
@@ -11,4 +11,47 @@ class BankAccount {
   // prepare withdraw logic in void witdraw function
   // prepare displayAccountDetails logic and ensure that it should contain current Balance and accountNumber in print statements
   // prepare getters and setters
+  public void deposit(double amount) {
+    if (amount > 0) {
+        balance += amount;
+        System.out.println(amount + " deposited successfully.");
+        displayAccountDetails();
+    } else {
+        System.out.println("Invalid amount. Deposit amount should be positive.");
+    }
+}
+public void withdraw(double amount) {
+    if (amount > 0) {
+        if (balance >= amount) {
+            balance -= amount;
+            System.out.println(amount + " withdrawn successfully.");
+            displayAccountDetails();
+        } else {
+            System.out.println("Insufficient balance.");
+        }
+    } else {
+        System.out.println("Invalid amount. Withdrawal amount should be positive.");
+    }
+}
+    public void displayAccountDetails() {
+        System.out.println("Account Number: " + accountNumber);
+        System.out.println("Current Balance: " + balance);
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+  
    }
