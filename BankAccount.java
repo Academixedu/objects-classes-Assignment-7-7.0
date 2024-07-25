3ed032a83a035bc712bdaaa12a8bcca29220e0a7
kavitha-guntru <kavithaguntru60@gmail.com>
2024-07-25 16:19:23 +0530

    kavitha

--- BankAccount.java
+++ BankAccount.java
@@ -7,8 +7,50 @@ class BankAccount {
         this.balance = initialBalance;
     }
 
-  // prepare deposit logic in void deposit function
-  // prepare withdraw logic in void witdraw function
-  // prepare displayAccountDetails logic and ensure that it should contain current Balance and accountNumber in print statements
+    public void deposit(int amount)
+    {
+        if(balance>=amount)
+        {
+            balance=balance+amount;
+            System.out.println(amount + " deposited Current balance is: " + balance);
+        }
+        else{
+            System.out.println("Invalid Deposit");
+        }
+
+    }
+    public void withdraw(int amount){
+        if(balance>=amount)
+        {
+            balance=balance-amount;     
+            System.out.println(amount + " withdraw Current balance is: " + balance);
+        }
+        else
+        {
+            System.out.println("Insufficient Balance:");
+        }
+    }
+    public void displayAccountDetails()
+    {
+        System.out.println("Acount Number:"+accountNumber);
+        System.out.println("Current Balance: " + balance);
+    }
+  
   // prepare getters and setters
-   }
+  public String getAccountNumber()
+  {
+    return accountNumber;
+  }
+  public void setAccountNumber(String accountNumber) 
+  {
+    this.accountNumber = accountNumber;
+  }
+  public double getBalance() 
+  {
+    return balance;
+  }
+  public void setBalance(double balance) 
+  {
+    this.balance = balance;
+  }
+}
--- Test.java
+++ Test.java
@@ -5,7 +5,20 @@ public class Test{
     // call the functions and print the deposit withdrawl and account details
     // prepare an object of Bank Account with paramaters
     // Call the Functions and print values
+    BankAccount acc = new BankAccount("",0);
+    System.out.println("\nDetails of my  Account Number1:");
+    acc.displayAccountDetails();
+    System.out.println("-----------------");
+    acc.setAccountNumber("UB127893333");
+    acc.setBalance(7000);
+    acc.deposit(600);
+    acc.withdraw(400);
 
-}
-
+    BankAccount acc1 = new BankAccount("UB127893655", 8000);
+    System.out.println("\nDetails of my Account Number2:");
+    acc1.displayAccountDetails();
+    System.out.println("-----------------");
+    acc1.deposit(900);
+    acc1.withdraw(600);
+  }
 }
