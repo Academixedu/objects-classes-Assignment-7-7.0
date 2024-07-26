851451684dfd36da53c7b24511913b7a29e6ca06
Bipasa-mohapatra <mohapatrabipasa2002@gmail.com>
2024-07-26 12:41:27 +0530

    complete

--- BankAccount.java
+++ BankAccount.java
@@ -6,6 +6,59 @@ class BankAccount {
         this.accountNumber = accountNumber;
         this.balance = initialBalance;
     }
+    public BankAccount(){
+        System.out.println(accountNumber);
+        System.out.println(balance);
+    }
+public void deposite(double amount){
+    if(amount > 0){
+        balance=balance+amount;
+        System.out.println("Deposite successful.New balance:$"+ balance);
+    }
+    else{
+        System.out.println("Deposit amount must be positive:");
+    }
+
+    }
+    public void withdraw(double amount)
+    {
+        if(amount > 0 && amount <= balance){
+            balance=balance-amount;
+            System.out.println("Withdrawal successful.New balance:$"+balance);
+        }
+        else if(amount > balance){
+            System.out.println("Insufficient balance for withdrawal.");
+        }
+        else{
+            System.out.println("Withdrawal amount must be positive");
+        }
+        }
+        public void displayAccountDetails(){
+            System.out.println("Account Number: " + accountNumber);
+            System.out.println("Current Balance:$"+ balance);
+        }
+        public String getAccountNumber(){
+            return accountNumber;
+        }
+        public void setAccountNumber(String accountNumbe){ 
+            this.accountNumber = accountNumber ;
+        }
+        public double getBalance(){
+            return balance;
+        }
+        public void setBalance(double balance){
+            this.balance = balance;
+        }
+        public static void main(String[] args)
+        {
+            BankAccount account =new BankAccount("1000500",1000.0);
+            account.displayAccountDetails();
+            account.deposite(600.0);
+            account.withdraw(200.0);
+            account.withdraw(2000.0);
+            account.displayAccountDetails();
+        }
+
 
   // prepare deposit logic in void deposit function
   // prepare withdraw logic in void witdraw function
--- Test.java
+++ Test.java
@@ -1,11 +1,22 @@
 public class Test{
 
   public static void main(String[]args){
+    BankAccount ba=new BankAccount();
+    ba.setAccountNumber("123456");
+    ba.setBalance(30000);
+    System.out.println("Account number is"+ba.getAccountNumber());
+    System.out.println("balance is"+ba.getBalance());
+    ba.deposite(3000);
+    ba.withdraw(10000);
+    ba.displayAccountDetails();
+    BankAccount a=new BankAccount("6753234", 400000);
+    a.deposite(30000);
+    a.withdraw(355779);
+    a.displayAccountDetails();
+
+}
+}
 // prepare an object of Bank Account with no paramaters and initalize the values using getters and setters
     // call the functions and print the deposit withdrawl and account details
     // prepare an object of Bank Account with paramaters
-    // Call the Functions and print values
-
-}
-
-}
+    // Call the Functions and print vaBankAccount ba=new BankAccount();
